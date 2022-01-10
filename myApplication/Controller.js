0edd1d4bb69c56da69ac7390ec322c9306d838e5
Pico758 <pkudlur@vibrenthealth.com>
2022-01-10 19:10:38 +0530

    Pico758: using ui router to create form  template

---
+++ myApplication/Controller.js
@@ -0,0 +1,53 @@
+app.controller('mainForm', function ($scope) {
+
+    $scope.invalid_age = true;
+     
+    //validate age
+    $scope.ageChange = function () {
+        var age = parseInt($scope.age);
+        if (age < 18) {
+            $scope.invalid_age = true;
+            $scope.invalidAgeMsg = "enter above 18";
+        }
+        else if (age > 100) {
+            $scope.invalid_age = true;
+            $scope.invalidAgeMsg = " enter below 100";
+        }
+        else {
+            $scope.invalid_age = false;
+        }
+    };
+
+
+
+
+    //check all fields are valid
+    $scope.submitForm = function () {
+
+        // check to make sure the form is completely valid
+        if ($scope.registerForm.$valid) {
+
+            if ($scope.invalid_age) {
+                alert('form is not ready');
+            }
+            else {
+                alert("Fields are properly inputted");
+            }
+
+        }
+        else {
+            alert('form is not ready');
+        }
+
+    };
+    // restrict future date
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    if (dd < 10) { dd = '0' + dd }
+    if (mm < 10) { mm = '0' + mm }
+    today = yyyy + '-' + mm + '-' + dd;
+    document.getElementById("birthday").setAttribute("max", today);
+    
+});
--- myApplication/app.js
+++ myApplication/app.js
@@ -1,56 +1,13 @@
-var app = angular.module('patientRegistration', []);
+var app = angular.module('patientRegistration', ['ngRoute']);
 
-//angular controller
-app.controller('mainForm', function ($scope) {
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
-    $scope.invalid_age = true;
+    $routeProvider
 
-    //validate age
-    $scope.ageChange = function () {
-        var age = parseInt($scope.age);
-        if (age < 18) {
-            $scope.invalid_age = true;
-            $scope.invalidAgeMsg = "enter above 18";
-        }
-        else if (age > 100) {
-            $scope.invalid_age = true;
-            $scope.invalidAgeMsg = " enter below 100";
-        }
-        else {
-            $scope.invalid_age = false;
-        }
-    };
+        .when('/', {
+            templateUrl: 'form.html',
+            controller: 'mainForm'
+        });
 
 
-
-
-    //check all fields are valid
-    $scope.submitForm = function () {
-
-        // check to make sure the form is completely valid
-        if ($scope.registerForm.$valid) {
-
-            if ($scope.invalid_age) {
-                alert('form is not ready');
-            }
-            else {
-                alert("Fields are properly inputted");
-            }
-
-        }
-        else {
-            alert('form is not ready');
-        }
-
-    };
-    // restrict future date
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1;
-    var yyyy = today.getFullYear();
-    if (dd < 10) { dd = '0' + dd }
-    if (mm < 10) { mm = '0' + mm }
-    today = yyyy + '-' + mm + '-' + dd;
-    document.getElementById("birthday").setAttribute("max", today);
-
-});
+}]);
