aae172a008452a2bccc5d5d9f39decf5b209cad2
Pico758 <pkudlur@vibrenthealth.com>
2022-01-11 18:57:22 +0530

    Pico758:patient rigistration age input calculation  updated  by using DOB input filed

--- myApplication/Controller.js
+++ myApplication/Controller.js
@@ -1,41 +1,13 @@
 app.controller('mainForm', function ($scope) {
-
-    $scope.invalid_age = true;
-     
-    //validate age
-    $scope.ageChange = function () {
-        var age = parseInt($scope.age);
-        if (age < 18) {
-            $scope.invalid_age = true;
-            $scope.invalidAgeMsg = "enter above 18";
-        }
-        else if (age > 100) {
-            $scope.invalid_age = true;
-            $scope.invalidAgeMsg = " enter below 100";
-        }
-        else {
-            $scope.invalid_age = false;
-        }
-    };
-
-
-
-
+    
     //check all fields are valid
     $scope.submitForm = function () {
 
         // check to make sure the form is completely valid
         if ($scope.registerForm.$valid) {
-
-            if ($scope.invalid_age) {
-                alert('form is not ready');
-            }
-            else {
-                alert("Fields are properly inputted");
-            }
-
-        }
-        else {
+            alert("Fields are properly inputted");
+           }
+         else {
             alert('form is not ready');
         }
 
@@ -49,5 +21,44 @@ app.controller('mainForm', function ($scope) {
     if (mm < 10) { mm = '0' + mm }
     today = yyyy + '-' + mm + '-' + dd;
     document.getElementById("birthday").setAttribute("max", today);
+
+    // age calculation
+    $scope.findage = function () {
+        var PresentDay = new Date();
+        var dateOfBirth = (new Date(document.getElementById("birthday").value));
+        var months = (PresentDay.getMonth() - dateOfBirth.getMonth() +
+            (12 * (PresentDay.getFullYear() - dateOfBirth.getFullYear())));
+        document.getElementById("getAge").value = Math.round(months / 12);
+    };
+
+
+ });
+
+
+
+
+
+    // $scope.invalid_age = true;
+
+    // // validate age
+    // $scope.ageChange = function () {
+    //     var age = parseInt($scope.age);
+    //     if (age < 18) {
+    //         $scope.invalid_age = true;
+    //         $scope.invalidAgeMsg = "enter above 18";
+    //     }
+    //     else if (age > 100) {
+    //         $scope.invalid_age = true;
+    //         $scope.invalidAgeMsg = " enter below 100";
+    //     }
+    //     else {
+    //         $scope.invalid_age = false;
+    //     }
+    // };
+
     
-});
+            // if ($scope.invalid_age) {
+            //     alert('form is not ready');
+            // }
+            // else {
+
